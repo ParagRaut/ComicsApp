5ea06797396f9e9c97cdbcbaff9f60d6aed9e22d
Parag <parag.raut@outlook.com>
2026-08-09 21:35:50 +0200

    restructured code

--- src/ComicsProvider/ServiceCollectionExtensions.cs
+++ src/ComicsProvider/ServiceCollectionExtensions.cs
@@ -11,7 +11,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddComicsService(this IServiceCollection services)
     {
-        services.AddRefitClient<IXKCDService>()
+        services.AddRefitGeneratedClient<IXKCDService>()
             .ConfigureHttpClient(client => client.BaseAddress = new Uri("https://xkcd.com"));
 
         services.AddScoped<XKCDService>();
